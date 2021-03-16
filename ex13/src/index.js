8b37eac18c154092ddec107e1b13568761f029e1
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-16 18:23:06 +0100

    ex13 While loops

---
+++ ex13/src/index.js
@@ -0,0 +1,15 @@
+    // Only change code below this line
+function inverseWhile(){
+    var fiveNumbers = 5;
+    while(fiveNumbers > 0){
+        console.log(fiveNumbers);
+        fiveNumbers--;
+    }
+    return fiveNumbers;
+}
+       
+    // Only change code above this line
+      
+    console.log(inverseWhile());
+    
+    module.exports = inverseWhile;
