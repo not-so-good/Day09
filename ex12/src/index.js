95f5dafefb5b544e916371f387abffecfea5aef6
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-16 19:29:57 +0100

    ex12 Multiple identical options in switch statements

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,21 +1,21 @@
 function switchCaseSequential(num){
     // Only change code below this line
-    var answer = " ";
-    switch(answer){
+    var answer;
+    switch(num){
         case 1:
         case 2:
         case 3:
-            result = "Low";
+            answer = "Low";
             break;
         case 4:
         case 5:
         case 6:
-            result = "Mid";
+            answer = "Mid";
             break;
         case 7:
         case 8:
         case 9:
-            result = "High"
+            answer = "High"
     }
 
     // Only change code below this line
