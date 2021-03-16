9f6e25b0ee24d7c801a0fff7c61c7b4296d15e1f
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-16 20:17:59 +0100

    ex14 For loops

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -1,6 +1,24 @@
-    // Only change code below this line
-    
-           
-        // Only change code above this line
+// Only change code below this line
+function myForLoop1(){
+    for(var evenNumbers = 0; evenNumbers <= 8; evenNumbers++){
+        if (evenNumbers % 2 == 0){
+            console.log(evenNumbers);
+        }
+        
+    }
+}
+function myForLoop2(){
+    for(var evenInverseNumbers = 8; evenInverseNumbers >= 0; evenInverseNumbers--){
+        if (evenInverseNumbers % 2 == 0){
+            console.log(evenInverseNumbers);
+        }
+    }
+}
+myForLoop1();
+myForLoop2();
+// Only change code above this line
           
-module.exports = inverseWhile;
+module.exports = {
+    myForLoop1,
+    myForLoop2
+};
