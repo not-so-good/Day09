a65ced3914056b115063798adc451a5f5cba3eed
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-16 19:29:28 +0100

    ex11 Switch statements

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,19 +1,19 @@
 function switchCase(letter){
 // Only change code below this line
-var animals = " ";
-    switch(animals){
-    case letter === "a":
-        animals = "antelope";
-        break;
-    case letter === "b":
-        animals = "bird";
-        break;
-    case letter === "c":
-        animals = "cat";
-        break;
-    default:
-        animals = "stuff";
-        break;
+    var animals = " ";
+    switch(letter){
+        case "a":
+            animals = "antelope";
+            break;
+        case "b":
+            animals = "bird";
+            break;
+        case "c":
+            animals = "cat";
+            break;
+        default:
+            animals = "stuff";
+            break;
     }
    
 // Only change code above this line
